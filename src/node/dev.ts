75c27a4a911a149eecc158c0674e3ed232b41dc6
Eternal-could <20578466@qq.com>
2024-06-29 20:50:28 +0800

    feat: Initializing Theme Components and add react and react-dom dependencies.

--- src/node/dev.ts
+++ src/node/dev.ts
@@ -1,9 +1,10 @@
 import { createServer } from 'vite';
 import {pluginIndexHtml} from './plugin-lms/indexHtml';
+import react from '@vitejs/plugin-react'
 
 export function createDevServer(root: string) {
   return createServer({
     root,
-    plugins: [pluginIndexHtml()]
+    plugins: [pluginIndexHtml(), react()]
   })
 }
---
+++ src/runtime/App.tsx
@@ -0,0 +1,5 @@
+import {Layout} from '../theme-default';
+const App = () => {
+  return <Layout/>
+}
+export default App
