4c7c5cb25c5f551cf86c7e4be6d74d2945447381
Eternal-could <20578466@qq.com>
2024-07-06 12:53:54 +0800

    perf(script)：script automatic import

--- src/node/plugin-lms/indexHtml.ts
+++ src/node/plugin-lms/indexHtml.ts
@@ -1,10 +1,25 @@
 import { Plugin } from 'vite';
 import { readFile } from 'fs/promises';
-import {DEFAULT_TEMPLATE_PATH} from '../constants';
+import {CLIENT_ENTRY_PATH, DEFAULT_TEMPLATE_PATH} from '../constants';
 
 export function pluginIndexHtml():Plugin {
   return {
     name: 'lms:index-html',
+    transformIndexHtml(html) {
+      return {
+        html,
+        tags: [
+          {
+            tag: 'script',
+            attrs: {
+              type: 'module',
+              src: `/@fs/${ CLIENT_ENTRY_PATH }`
+            },
+            injectTo: 'body'
+          }
+        ]
+      }
+    },
     configureServer(server) {
       return () => {
         server.middlewares.use(async (req, res, next)=>{
@@ -12,8 +27,8 @@ export function pluginIndexHtml():Plugin {
             // 1. 读取 template.html 的文件内容
             let html = await readFile(DEFAULT_TEMPLATE_PATH, 'utf-8')
             // 2. 响应 HTML 浏览器
-            // res.setHeader('Content-Type', 'text/html')
-            html = await server.transformIndexHtml(req.url, html)
+            html = await server.transformIndexHtml(req.url, html, req.originalUrl)
+            res.setHeader('Content-Type', 'text/html')
             res.end(html)
           } catch (e) {
             return next(e)
